1133ceae2e05c021f48e4c765f8f39f0e14d10ff
Antanu Dutta <antanu.bittu@gmail.com>
2025-08-09 13:58:52 +0530

    new project 'contact-app'

---
+++ contact-app/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import { Contact } from "./models/contacts.models.js";
+
+const app = express();
+const PORT = 3000;
+
+// database connection
+mongoose
+  .connect(
+    `mongodb+srv://antanubittu:L21V20Pmqs6XnF1s@backendusingai.qo2xcn8.mongodb.net/user-contact?retryWrites=true&w=majority&appName=backendusingai`
+  )
+  .then(() => console.log("Database connected"));
+
+// Get __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", async (req, res) => {
+  const contacts = await Contact.find({});
+  res.render("home", { contacts });
+});
+
+app.get("/show-contact/:id", async (req, res) => {
+  const _id = req.params.id;
+  const user = await Contact.findOne({ _id });
+  res.render("show-contact", { user });
+});
+app.get("/add-contact", (req, res) => {
+  res.render("add-contact");
+});
+app.post("/add-contact", (req, res) => {});
+app.get("/update-contact", (req, res) => {
+  res.render("update-contact");
+});
+app.post("/update-contact/:id", (req, res) => {});
+app.get("/delete-conttact", (req, res) => {});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ contact-app/models/contacts.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+    },
+    last_name: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Contact = mongoose.model("Contact", contactSchema);
